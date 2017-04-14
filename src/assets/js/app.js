fb826898cf0bc50acbf6ed8cd74de668dd88bec8
Woshiajuana <zhigang.chen@owulia.com>
2017-04-14 18:43:46 +0800

    优化的代码

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/4/1.
  */
-import Vue from 'vue'                                       /**引入vue*/
+
 import MyHeader from '../../components/My-Header.vue'       /**引入头部*/
 import MyNav from '../../components/My-Nav.vue'             /**引入导航条*/
 import SvgTemplate from '../../components/Svg.vue'          /**引入svg图标*/
